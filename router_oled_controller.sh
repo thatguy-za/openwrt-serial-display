8521b15e7cbd9c4af67b835a8222b238d76aa675
Tristam <69636837+thatguy-za@users.noreply.github.com>
2023-05-30 07:30:24 +0100

    Update router_oled_controller.sh
    Updated to include CPU (thermal zone 0) temp

--- router_oled_controller.sh
+++ router_oled_controller.sh
@@ -4,5 +4,6 @@ hostname=$(uci get system.@system[0].hostname)
 wan_ip=$(. /lib/functions/network.sh; network_find_wan NET_IF; network_get_ipaddr NET_ADDR "${NET_IF}"; echo "${NET_ADDR}")
 lan_ip=$(uci get network.lan.ipaddr)
 wifi_strength=$(cat /proc/net/wireless | awk 'NR==3 {print $4}' | sed 's/\.//')
+let cpu_temp=$(cat /sys/class/thermal/thermal_zone0/temp)/1000
 
-echo $hostname "|" WAN IP: $wan_ip "|" LAN IP $lan_ip "|" WAN WiFi: $wifi_strength dbm > /dev/ttyAMA0
+echo $hostname "|" WAN:$wan_ip "|" LAN:$lan_ip "|" WAN:$wifi_strength dbm CPU:$cpu_temp c > /dev/ttyAMA0
